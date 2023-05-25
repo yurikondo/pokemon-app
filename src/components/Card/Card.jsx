e145520c3db420afbb09cb8c0eee7fe6c380a116
yuri <yuri@example.com>
2023-05-26 08:49:55 +0900

    15. ポケモンデータを実際のページに出力してみよう

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Card = ({ pokemon }) => {
-  return <div>Card</div>;
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes ">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">重さ：{pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="height">高さ：{pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="abilities">
+            アビリティ：{pokemon.abilities[0].ability.name}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
