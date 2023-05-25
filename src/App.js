88c3d2d0236b41f6d9aeb9c90be716c15f0c129b
yuri <yuri@example.com>
2023-05-26 08:38:26 +0900

    14. Cardコンポーネントを作成しよう

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./components/Card/Card";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -33,7 +34,21 @@ function App() {
   };
   return (
     <div className="App">
-      {loading ? <h1>ロード中✨</h1> : <h1>ポケモンデータを取得しました</h1>}
+      {loading ? (
+        <h1>ロード中✨</h1>
+      ) : (
+        <>
+          <div className="pokemonCardContainer">
+            {pokemonDate.map((pokemon, i) => {
+              return (
+                <Card key={i} pokemon={pokemon}>
+                  Pokemon{" "}
+                </Card>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
